c4b3fd037f81a96e6c21173edfb663de0ac1d04f
cleverclope <cleverarinda@gmail.com>
2023-01-12 07:18:13 +0300

    added few screens

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
